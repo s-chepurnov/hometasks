9c86a5d7fb5eb2628f5486be106336aa86a86849
root <root@debian>
2018-02-18 15:18:55 +0300

    PoW

--- src/main/scala/mining/PoWMiner.scala
+++ src/main/scala/mining/PoWMiner.scala
@@ -8,7 +8,17 @@ class PoWMiner[HF <: CryptographicHash32](hashFunction: HF) {
 
   private val MaxTarget: BigInt = BigInt(1, Array.fill(32)((-1).toByte))
 
-  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = ???
+  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = {
+    var nonce = 0
+    var provedData: ProvedData = new ProvedData(data , nonce)
+
+    while ( !validateWork(provedData, difficulty)) {
+      nonce = nonce + 1
+      provedData = new ProvedData(data , nonce)
+    }
+
+    provedData
+  }
 
   def validateWork(data: ProvedData, difficulty: BigInt): Boolean = realDifficulty(data) >= difficulty
 
